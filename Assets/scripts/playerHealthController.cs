8ca623ec8ff5ed6158e70297b4c0250c510c97e0
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-05-16 10:39:48 +0530

    added health pickups

--- Assets/scripts/playerHealthController.cs
+++ Assets/scripts/playerHealthController.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class playerHealthController : MonoBehaviour
 {
+    public static playerHealthController instance;
     public float currentHealth ;
     public float maxHealth = 50f;
 
+    void Awake(){
+        instance = this;
+    }
     void Start (){
         currentHealth = maxHealth;
         uiController.instance.healthSlider.maxValue = maxHealth;
@@ -17,17 +21,26 @@ public class playerHealthController : MonoBehaviour
     }
     public void damagePlayer(float dmgAmount){
         currentHealth -= dmgAmount;
-        uiController.instance.healthSlider.value = currentHealth;
-        uiController.instance.healthText.text = "HEALTH: "+ currentHealth +"/"+ maxHealth;
-        if (currentHealth <= (0.42069)*maxHealth)
-        {
-            uiController.instance.anim.SetBool("isHealthLow",true);
-        }
+        updateHealthUi();
+       
         if(currentHealth <= 0) 
         {
             gameObject.SetActive(false);
             gameManager.instance.restartGame();
         }
     }  
+    public void maxHealPlayer(){
+        currentHealth = maxHealth;
+        updateHealthUi();
+    }
+    public void updateHealthUi(){
+        uiController.instance.healthSlider.value = currentHealth;
+        uiController.instance.healthText.text = "HEALTH: "+ currentHealth +"/"+ maxHealth;
+         if (currentHealth <= (0.42069)*maxHealth)
+        {
+            uiController.instance.anim.SetBool("isHealthLow",true);
+        }else uiController.instance.anim.SetBool("isHealthLow",false);
+
+    }
 }   
 
