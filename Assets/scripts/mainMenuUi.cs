29eb9be531a2fac5f9d9c1e846a0de9207fed408
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-05-20 17:51:31 +0530

    added alt menu

--- Assets/scripts/mainMenuUi.cs
+++ Assets/scripts/mainMenuUi.cs
@@ -6,16 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class mainMenuUi : MonoBehaviour
 {
+        public List<Button> allBtns = new List<Button>();
+        public int highLightedBtn;
+         public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        highLightedBtn = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            highLightBtn();
+            highLightedBtn++;
+            if(highLightedBtn == 2) highLightedBtn = 0;
+        }
     }
     public void exit(){
         Application.Quit();
@@ -24,4 +32,8 @@ public class mainMenuUi : MonoBehaviour
     public void LoadLvl1(){
         SceneManager.LoadScene("buildLvl");
     }
+    public void highLightBtn(){
+      allBtns[highLightedBtn].animator.SetTrigger("Highlighted");
+    }
+    
 }
