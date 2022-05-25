0996c7fa8ea3273a83c0b6c2820f42e42b61574a
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-05-25 19:43:55 +0530

    added exploder and other enemy Fx

---
+++ Assets/scripts/enemyHealthSystemCommon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealthSystemCommon : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public float dmgToBeTaken;
+    public float lowHealthIndicationNo;
+    public ParticleSystem effectUponLowHealth;
+    public ParticleSystem effectUponDestroy;
+    public bool hasLowHealthFx;
+    private bool lowHealthFxOn;
+    private bool destroyFxOn;
+    public bool hasDestroyFx;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        lowHealthFxOn = false;
+        destroyFxOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth <=lowHealthIndicationNo && hasLowHealthFx && lowHealthFxOn == false){
+            Instantiate(effectUponLowHealth,transform.position,transform.rotation);
+        }
+        if(currentHealth <= 0 && destroyFxOn == false){
+            lowHealthFxOn = true;
+            Destroy(gameObject);
+            if(hasDestroyFx)
+            Instantiate(effectUponDestroy,transform.position,transform.rotation);
+            destroyFxOn = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other){
+       if(other.gameObject.tag == "playerBullet"){
+           currentHealth -= dmgToBeTaken;
+        }
+    }
+}
